659103ef469dd1fe0bbbc953606e3ad64e84efb6
kevinmont <live-mont@outlook.com>
2019-12-21 12:56:50 -0600

    Initial Commit

---
+++ kenglish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kenglish
+//
+//  Created by Kevin Montalvo on 21/12/19.
+//  Copyright © 2019 Kevin Montalvo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
